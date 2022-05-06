b77d1b5344d3eb3dba46cd93db07e59122a5e761
vivat <neil4884@gmail.com>
2022-05-06 18:27:18 +0700

    Add some notes for future changes

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 if __name__ == '__main__':
     print('Welcome')
+    # TO_DO this : config
+    # for ground station and rocket software: add hot_cmd functionality to overwrite eeprom on the fly
+    # also add default readline mode in serial if data_format is not present
